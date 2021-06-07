9789b8c271cb0598d7cebdab80ca2ea80e5d5c5b
belausabaev <b.usabaev@khm.de>
2021-06-07 12:56:04 +0200

    add granular sound to condegram

--- main.js
+++ main.js
@@ -1,8 +1,36 @@
+    // ---------- Audio Context ------------------//
+
+    //create audio context for all theremin voices
+    ctx = new (AudioContext || webkitAudioContext)();
+    ctx.suspend();
+    var contour = ctx.createGain();
+    //initialize audio context for grainsynth
+
+    init(ctx);
+    grainSample = 0; // 0 = synthetic sound, 2 = guitar sound, 3 = piano with echo sound
+    bufferSwitch(grainSample);
+    grainPlaying = false;
+
+    // initialize default theremin sound
+    oscillator = null;
+    gainNode = ctx.createGain();
+    gainNode.gain.value = 0.5;
+    var soundPlaying = false;
+
+
+   // posY = map(poses.leftWrist.y, 0, video.height, 0, window.innerHeight);
+   //         posX = map(poses.rightWrist.x, 0, video.width, 0, window.innerWidth);
+   //         grains(posX, posY);
+
+
+
+
+
 var width = 500,
     height = 500,
     start = 0,
     end = 2.25,
-    numSpirals = 3
+    numSpirals = 2
     margin = {top:50,bottom:50,left:50,right:50};
 
 var theta = function(r) {
@@ -83,7 +111,7 @@ svg.selectAll("rect")
         angleOnLine = path.node().getPointAtLength(linePer - barWidth);
     
     d.linePer = linePer; // % distance are on the spiral
-    d.x = posOnLine.x; // x postion on the spiral
+    d.x = posOnLine.x; // x position on the spiral
     d.y = posOnLine.y; // y position on the spiral
     
     d.a = (Math.atan2(angleOnLine.y, angleOnLine.x) * 180 / Math.PI) - 90; //angle at the spiral position
@@ -164,6 +192,10 @@ svg.selectAll("rect")
     tooltip.select('.date').html("Date: <b>" + d.date.toDateString() + "</b>");
     tooltip.select('.value').html("Value: <b>" + Math.round(d.value*100)/100 + "<b>");
 
+    // load random sound sample of available (4) samples and granulate according to mouse/data bar coordinates
+    loadSample(randSample());
+    grains(event.clientX,event.clientY);
+
     d3.select(this)
     .style("fill","#FFFFFF")
     .style("stroke","#000000")
@@ -176,6 +208,8 @@ svg.selectAll("rect")
 .on('mousemove', function(d) {
     tooltip.style('top', (d3.event.layerY + 10) + 'px')
     .style('left', (d3.event.layerX - 25) + 'px');
+
+    grains(event.clientX,event.clientY);
 })
 .on('mouseout', function(d) {
     d3.selectAll("rect")
@@ -185,3 +219,103 @@ svg.selectAll("rect")
     tooltip.style('display', 'none');
     tooltip.style('opacity',0);
 });
+
+
+
+    // GUI for grain params
+    // Documentation: https://cocopon.github.io/tweakpane/input.html
+
+    const PARAMS = {
+        source: 0,
+        attack: 0.8,
+        decay: 0.8,
+        density: 35,
+        start: 0,
+        end: 0,
+    };
+
+    const pane = new Tweakpane({
+        title: 'VIRTUAL THEREMIN SOUNDS',
+        expanded: true,
+    });
+
+    pane.addSeparator();
+
+    const gs = pane.addFolder({
+        title: 'THEREMIN GRANULAR SYNTHESIS',
+        expanded: true,
+    });
+
+    const btn = gs.addButton({
+        title: '► | ◼︎',
+        label: 'sound',
+    });
+
+    btn.on('click', () => {
+        console.log(ctx.state);
+        if (ctx.state === 'running') {
+                ctx.suspend();
+                if (grainPlaying) {
+                    grainGain.disconnect();
+                    grainPlaying = false;
+                }
+        } else if (ctx.state === 'suspended') {
+            ctx.resume().then(function () {
+                // start grains so some sound is coming when sound on button is clicked
+                grainGain = ctx.createGain();
+                grainGain.connect(ctx.destination);
+                bufferSwitch(grainSample);
+                grainPlaying = true;
+            });
+        }
+    });
+
+ //   const SourceInput = gs.addInput(PARAMS, 'source', { options: { Synthetic_Sound: 0, Guitar: 1, Piano: 2 , Orchestra_Tuning: 3} });
+ //   SourceInput.on('change', function (ev) {
+ //       grainSample = ev.value;
+ //       bufferSwitch(grainSample);
+ //   });
+
+    const f = gs.addFolder({
+        title: 'GRAIN SETTINGS',
+        expanded: true,
+    });
+
+    const attackInput = f.addInput(PARAMS, 'attack', { min: 0.01, max: 1, step: 0.01 });
+    attackInput.on('change', function (ev) {
+        // change something
+        //console.log(ev.value.toFixed(2));
+        att = parseFloat(ev.value.toFixed(2)); // parse incoming value for grainmachine.js
+    });
+
+    const decayInput = f.addInput(PARAMS, 'decay', { min: 0.01, max: 1, step: 0.01 });
+    decayInput.on('change', function (ev) {
+        // change something
+        dec = parseFloat(ev.value.toFixed(2)); // parse incoming value for grainmachine.js
+    });
+/*
+    const densityInput = f.addInput(PARAMS, 'density', { min: 10, max: 500, step: 5 });
+    densityInput.on('change', function (ev) {
+        // change something
+        rate = parseFloat(ev.value.toFixed());
+    });
+*/
+    pane.addSeparator();
+
+    const instr = pane.addFolder({
+        title: 'THEREMIN CLASSIC',
+    });
+
+
+
+function randSample() {
+    const rndInt = Math.floor(Math.random() * 4);
+    console.log("rand int " + rndInt);
+    return rndInt; 
+}
+
+function loadSample(s) {
+   // if(grainPlaying){
+        bufferSwitch(s);
+   // }
+}
